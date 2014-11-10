82d0328d6cebb9a2d8495b4b0998c43d3d2d4e37
Samantha McCormick <samanthagmccormick@gmail.com>
2014-11-09 22:06:39 -0700

    Styling dashboards, and made volunteer table for the event manager dashboard

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,8 @@
+/* Table Filter */
+
+
+
+
 // CLIENT-SIDE - This is where jQuery goes!
 
 console.log('hello!!!');
@@ -15,5 +20,48 @@ $(document).ready(function() {
         $(".bhoechie-tab>.bhoechie-tab-content").removeClass("active");
         $(".bhoechie-tab>.bhoechie-tab-content").eq(index).addClass("active");
     });
+
+/* TABLE FILTER */
+
+    'use strict';
+
+    var LightTableFilter = (function(Arr) {
+
+        var _input;
+
+        function _onInputEvent(e) {
+            _input = e.target;
+            var tables = document.getElementsByClassName(_input.getAttribute('data-table'));
+            Arr.forEach.call(tables, function(table) {
+                Arr.forEach.call(table.tBodies, function(tbody) {
+                    Arr.forEach.call(tbody.rows, _filter);
+                });
+            });
+        }
+
+        function _filter(row) {
+            var text = row.textContent.toLowerCase(), val = _input.value.toLowerCase();
+            row.style.display = text.indexOf(val) === -1 ? 'none' : 'table-row';
+        }
+
+        return {
+            init: function() {
+                var inputs = document.getElementsByClassName('light-table-filter');
+                Arr.forEach.call(inputs, function(input) {
+                    input.oninput = _onInputEvent;
+                });
+            }
+        };
+    })(Array.prototype);
+
+    document.addEventListener('readystatechange', function() {
+        if (document.readyState === 'complete') {
+            LightTableFilter.init();
+        }
+    });
+
+
+
+
 
 }); // end jQuery ready
